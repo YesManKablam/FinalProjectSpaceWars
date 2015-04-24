c4f3f8a65f23caa56070e9aec5bae1dea6bff20b
YesMAnKablam <conorkenny183@gmail.com>
2015-04-24 22:03:53 +0100

    Initial Push

---
+++ Assets/Scripts/PlayerDeath.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerDeath : MonoBehaviour {
+	
+	public int damage = 0;
+	public float invul = 0;
+	SpriteRenderer sr;
+	public GameObject spawner;
+	public GameObject player;
+
+	void Start (){
+		spawner = GameObject.Find ("EnemySpawn");
+		sr = GetComponent<SpriteRenderer> ();
+
+		if (sr == null) {
+			Debug.LogError("The ' "+gameObject.name+"' has no sprite renderer");
+		}
+	}
+
+	void OnCollisionEnter2D(){
+		damage += 1;
+		invul += 10;
+		gameObject.layer = 10;
+		int currScore;
+		if (damage >= 3) {
+			//currScore = spawner.GetComponent<EnemySpawnDynamic> ().score;
+//			if (currScore == 0) //{
+				//currScore = spawner.GetComponent<EnemySpawnerHard> ().score;
+			//} else if (currScore == 0) {
+			//	currScore = spawner.GetComponent<EnemySpanwerMedium> ().score;
+		//	}
+
+		//	PlayerPrefs.SetInt ("currentScore", currScore);
+			//Debug.Log ("Player Prefs score: " + PlayerPrefs.GetInt ("currentScore"));
+			Destroy (player);
+			Debug.Log("Dead");
+		//}
+	}
+}
+
+}
---
+++ Assets/Scripts/PlayerSpawner.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerSpawner : MonoBehaviour {
+
+	public GameObject playerPrefab;
+	GameObject playerInstance;
+	public float respawnTimer = 2;
+	public float tmpSpawn;
+	public int numDeath = 0;
+
+	// Use this for initialization
+	void Start () {
+		spawnPlayer ();
+		tmpSpawn = respawnTimer;
+	}	
+
+	void spawnPlayer()
+	{
+		numDeath ++;
+		respawnTimer = 1;
+		playerInstance = (GameObject)Instantiate (playerPrefab, transform.position, Quaternion.identity);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if (playerInstance == null) {
+			respawnTimer -= Time.deltaTime;
+
+			if(respawnTimer <= 0){
+				spawnPlayer();
+			}
+
+			if(numDeath == 3)
+			{
+				Application.LoadLevel("_Scene_4");
+			}
+		}
+	
+	}
+}
